c6143b0617b3c2484382e139b3ec2bc6868dc95d
Eric Schoonover <erics@capdes.com>
2010-04-29 00:56:12 -0700

    Added advanced function: Save-DataSource

--- Semantic-PoSH.psm1
+++ Semantic-PoSH.psm1
@@ -8,7 +8,8 @@ loadPrerequisiteAssembly "RdfCore" (Get-Item ./bin/RdfCore.dll)
 . ./Core/New-DataSource.ps1
 . ./Core/ConvertTo-RdfValue.ps1
 . ./Core/Restore-RdfObject.ps1
+. ./Core/Save-DataSource.ps1
 
 Pop-Location
 
-Export-ModuleMember -Function @('New-DataSource', 'ConvertTo-RdfValue', 'Restore-RdfObject')
+Export-ModuleMember -Function @('New-DataSource', 'ConvertTo-RdfValue', 'Restore-RdfObject', 'Save-DataSource')
